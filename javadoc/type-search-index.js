ecfd4c093d0b5544e84af3e6121dd4cf534fea77
astephant.ira2026 <astephant.ira2026@esaip.org>
2023-12-21 17:43:14 +0100

    Added Javadoc to Github

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example","l":"Canard"},{"p":"org.example","l":"Elevage"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Poulet"},{"p":"org.example","l":"Volaille"}];updateSearchResults();
